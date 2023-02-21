45c6b3ee8004f03954b3dc5739dbb90407932db8
pedro <p.barreiro@kigroup.de>
2023-02-21 09:22:36 +0000

    feat: add swagger ui to project

--- internal/api/Router/router.go
+++ internal/api/Router/router.go
@@ -5,11 +5,20 @@ import (
 	"github.com/Pedrommb91/openapi-example/internal/api/service"
 	"github.com/Pedrommb91/openapi-example/pkg/logger"
 	"github.com/gin-gonic/gin"
+	"github.com/go-openapi/runtime/middleware"
 )
 
 func NewRouter(handler *gin.Engine, l logger.Interface) {
 	handler.Use(gin.Logger())
 	handler.Use(gin.Recovery())
+
+	handler.StaticFile("/swagger", "./spec/openapi.yaml")
+
+	opts := middleware.SwaggerUIOpts{SpecURL: "/swagger", Path: "/swagger/ui"}
+	sh := middleware.SwaggerUI(opts, nil)
+	handler.GET("/swagger/ui", func(ctx *gin.Context) {
+		sh.ServeHTTP(ctx.Writer, ctx.Request)
+	})
 
 	mid := make([]openapi.MiddlewareFunc, 0)
 	opt := openapi.GinServerOptions{
--- spec/gen.go
+++ spec/gen.go
@@ -2,3 +2,4 @@ package spec
 
 //go:generate oapi-codegen --config=gin_cfg.yaml openapi.yaml
 //go:generate oapi-codegen --config=genres_types_cfg.yaml openapi.yaml
+//go:generate oapi-codegen --config=swagger_cfg.yaml openapi.yaml
